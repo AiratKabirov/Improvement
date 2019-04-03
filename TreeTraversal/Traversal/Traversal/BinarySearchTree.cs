4cd622e3c179e6dc765234aaaf6d2c31e18b3152
Airat Kabirov <kabirov.airat@inbox.ru>
2019-04-03 14:19:56 -0700

    Tree deserialization added

---
+++ TreeTraversal/Traversal/Traversal/BinarySearchTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Traversal
+{
+    public class BinarySearchTree
+    {
+        public void PrintInorder(TreeNode root)
+        {
+            if (root == null) return;
+
+            PrintInorder(root.LeftChild);
+            Console.WriteLine( " " + root.NodeValue + "," );
+            PrintInorder(root.RightChild);
+        }
+
+        public void PrintPreorder(TreeNode root)
+        {
+            if (root == null) return;
+
+            Console.WriteLine(" " + root.NodeValue + ",");
+            PrintPreorder(root.LeftChild);
+            PrintPreorder(root.RightChild);
+        }
+
+        public TreeNode DeserializeTreeOptimized(int[] preorder, int[] currIndex, int min, int max)
+        {
+            if (currIndex[0] >= preorder.Length) return null;
+
+            TreeNode root = null;
+
+            if ((preorder[currIndex[0]] > min) && (preorder[currIndex[0]] < max))
+            {
+                root = new TreeNode(preorder[currIndex[0]]);
+                currIndex[0] += 1;
+                root.LeftChild = DeserializeTreeOptimized(preorder, currIndex, min, root.NodeValue);
+                root.RightChild = DeserializeTreeOptimized(preorder, currIndex, root.NodeValue, max);
+            }
+
+            return root;
+        }
+
+        public int FindDivision(int[] preorder, int value, int low, int high)
+        {
+            int i;
+            for (i = low; i <= high; i++)
+            {
+                if (value < preorder[i])
+                    break;
+            }
+            return i;
+        }
+
+        public TreeNode DeserializeArray(int[] preorder, int low, int high)
+        {
+            if (low > high) return null;
+
+            TreeNode root = new TreeNode(preorder[low]);
+
+            int divIndex = FindDivision(preorder, root.NodeValue, low + 1, high);
+
+            root.LeftChild = DeserializeArray(preorder, low + 1, divIndex - 1);
+            root.RightChild = DeserializeArray(preorder, divIndex, high);
+
+            return root;
+        }      
+    }
+}
---
+++ TreeTraversal/Traversal/Traversal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+        5
+  2            7
+1   3        6    8
+      4
+*/
+
+            int[] preorder = { 5, 2, 1, 3, 4, 7, 6, 8 };
+
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            int[] currIndex = new int[1];
+            currIndex[0] = 0;
+
+            int min = int.MinValue;
+            int max = int.MaxValue;
+
+            TreeNode root = binarySearchTree.DeserializeTreeOptimized(preorder, currIndex, min, max);
+
+            // TreeNode root = solution.deserializeArray(preorder, 0, preorder.length - 1);
+
+            Console.WriteLine("Inorder array for constructed BST is:");
+            binarySearchTree.PrintInorder(root);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Preorder array for constructed BST is:");
+            binarySearchTree.PrintPreorder(root);
+        }
+    }
+}
