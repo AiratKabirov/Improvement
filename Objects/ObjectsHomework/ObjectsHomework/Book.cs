2ff4290ff48b78fd84808466ee7744a4396c7b21
Airat Kabirov <kabirov.airat@inbox.ru>
2019-04-10 12:49:07 -0700

    Objects homework is implemented.

---
+++ Objects/ObjectsHomework/ObjectsHomework/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ObjectsHomework
+{
+    public class Book
+    {
+        public readonly Guid Id;
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        public Book(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var book = obj as Book;
+
+            if (book == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return this.Id == book.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ Objects/ObjectsHomework/ObjectsHomework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectsHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionaryOfBooks = new Dictionary<Book, string>();
+            Book bookToCheck = null;
+
+            for (int i = 0; i < 20; i++)
+            {
+                var book = new Book(Guid.NewGuid());
+                book.Name = i.ToString();
+
+                if (i == 10)
+                {
+                    bookToCheck = book;
+                }
+
+                dictionaryOfBooks.Add(book, book.Name);
+            }
+
+            Console.WriteLine("Book to check: " + bookToCheck.Name);
+            Console.WriteLine("Book from dictionary: " + dictionaryOfBooks[bookToCheck]);
+        }
+    }
+}
