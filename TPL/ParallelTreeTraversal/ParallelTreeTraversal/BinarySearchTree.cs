a5fdea1995019db3a2ce729b8c63e94ec0e287a6
Airat Kabirov <kabirov.airat@inbox.ru>
2019-04-18 12:45:26 -0700

    Parallel tree processing implemented.

---
+++ TPL/ParallelTreeTraversal/ParallelTreeTraversal/BinarySearchTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelTreeTraversal
+{
+    public class BinarySearchTree
+    {
+        public void ProcessTreeInParallel(TreeNode root, Action<int> action)
+        {
+            if (root == null) return;
+
+            Parallel.Invoke(
+                () => ProcessTreeInParallel(root.LeftChild, action),
+                () => ProcessTreeInParallel(root.RightChild, action),
+                () => action(root.NodeValue)
+                );
+        }
+
+        public TreeNode DeserializePostorderTreeOptimized(int[] postorder, ref int currIndex, int min, int max)
+        {
+            if (currIndex < 0) return null;
+
+            TreeNode root = null;
+
+            if ((postorder[currIndex] > min) && (postorder[currIndex] < max))
+            {
+                root = new TreeNode(postorder[currIndex]);
+                currIndex -= 1;
+                root.RightChild = DeserializePostorderTreeOptimized(postorder, ref currIndex, root.NodeValue, max);
+                root.LeftChild = DeserializePostorderTreeOptimized(postorder, ref currIndex, min, root.NodeValue);
+            }
+
+            return root;
+        }
+    }
+}
---
+++ TPL/ParallelTreeTraversal/ParallelTreeTraversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParallelTreeTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+        5
+  2            7
+1   3        6    8
+      4
+*/
+
+            int[] postorder = { 1, 4, 3, 2, 6, 8, 7, 5 };
+
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            int currIndexPostorder = postorder.Length - 1;
+
+            int min = int.MinValue;
+            int max = int.MaxValue;
+
+            TreeNode rootPostorder = binarySearchTree.DeserializePostorderTreeOptimized(postorder, ref currIndexPostorder, min, max);
+
+            Console.WriteLine("Postorder array for constructed BST is:");
+            binarySearchTree.ProcessTreeInParallel(rootPostorder, x => Console.WriteLine("My value is: " + x));
+        }
+    }
+}
