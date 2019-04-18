16111bb88560382a5646eb44ae09b17c36f26b66
Airat Kabirov <kabirov.airat@inbox.ru>
2019-04-18 11:12:08 -0700

    Thread synchronization homework implemented.

---
+++ ThreadSyncronization/ThreadSync/ThreadSync/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSync
+{
+    class Program
+    {
+        static Mutex globalMutex;
+
+        static bool IsSingleInstance()
+        {
+            try
+            {
+                // Try to open existing mutex.
+                Mutex.OpenExisting("ThreadSync");
+            }
+            catch
+            {
+                // If exception occurred, there is no such mutex.
+                Program.globalMutex = new Mutex(true, "ThreadSync");
+
+                // Only one instance.
+                return true;
+            }
+            // More than one instance.
+            return false;
+        }
+
+        static void Main()
+        {
+            if (!Program.IsSingleInstance())
+            {
+                Console.WriteLine("More than one instance"); // Exit program.
+            }
+            else
+            {
+                Console.WriteLine("One instance"); // Continue with program.
+                ThreadRunner tR = new ThreadRunner(6);
+            }
+
+            globalMutex.ReleaseMutex();
+            globalMutex.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ThreadSyncronization/ThreadSync/ThreadSync/ThreadRunner.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadSync
+{
+    public class ThreadRunner
+    {
+        ManualResetEvent manual = new ManualResetEvent(false);
+        AutoResetEvent auto = new AutoResetEvent(false);
+
+        public ThreadRunner(int i)
+        {
+            var range = Enumerable.Range(1, i);
+            range.ToList().ForEach(num =>
+            {
+                new Thread(new ParameterizedThreadStart(Work)).Start(num);
+                Thread.Sleep(10);
+            });
+        }
+
+        private void Work(object i)
+        {
+            var threadNum =(int)i;
+            switch(i)
+            {
+                case 1:
+                    Console.WriteLine("Thread 1 started");
+                    Thread.Sleep(100);
+                    Console.WriteLine("Thread 1 set signal");
+                    manual.Set();
+                    Thread.Sleep(50);
+                    Console.WriteLine("Thread 1 reset signal");
+                    manual.Reset();
+                    break;
+                case 2:
+                    Console.WriteLine("Thread 2 started");
+                    Thread.Sleep(50);
+                    Console.WriteLine("Thread 2 set signal");
+                    auto.Set();
+                    Thread.Sleep(100);
+                    Console.WriteLine("Thread 2 set signal");
+                    auto.Set();
+                    Thread.Sleep(50);
+                    break;
+                case 3:
+                    Console.WriteLine("Thread 3 is waiting for manual signal from Thread 1");
+                    manual.WaitOne();
+                    Console.WriteLine("Thread 3 received manual signal, continue working");
+                    break;
+                case 4:
+                    Console.WriteLine("Thread 4 is waiting for manual signal from Thread 1");
+                    manual.WaitOne();
+                    Thread.Sleep(5);
+                    Console.WriteLine("Thread 4 received manual signal, continue working");
+                    break;
+                case 5:
+                    Console.WriteLine("Thread 5 is waiting for auto signal from Thread 2");
+                    auto.WaitOne();
+                    Console.WriteLine("Thread 5 received auto signal, continue working");
+                    break;
+                case 6:
+                    Console.WriteLine("Thread 6 is waiting for auto signal from Thread 2");
+                    auto.WaitOne();
+                    Console.WriteLine("Thread 6 received auto signal, continue working");
+                    break;
+            }
+        }
+    }
+}
